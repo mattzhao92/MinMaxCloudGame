8d46e389ebd8dd11a2b37f75fe0043dadade58f0
Carolyn Shuford <sockcreature@gmail.com>
2014-05-01 21:41:21 -0500

    basic comments on playermap

--- src/Model/PlayerMap.java
+++ src/Model/PlayerMap.java
@@ -12,6 +12,11 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 
 import Json.GameInfo;
 
+/**
+ * A persistent HashMap of Player string to GameInfo
+ * @author CShuford
+ *
+ */
 @PersistenceCapable
 public class PlayerMap{
 	
@@ -25,23 +30,45 @@ public class PlayerMap{
 	@Persistent(serialized = "true", defaultFetchGroup = "true")
 	HashMap<String, GameInfo> players;
 	
+	/**
+	 * Constructor
+	 */
 	public PlayerMap() {
 		if (this.players == null)
 			this.players = new HashMap<String, GameInfo>();
 	}
 	
+	/**
+	 * Returns the string, gameinfo hashmap
+	 * @return
+	 */
 	public HashMap<String, GameInfo> getPlayerMap() {
 		return this.players;
 	}
 	
+	/**
+	 * Returns true if key is a key in the hashmap
+	 * @param key
+	 * @return
+	 */
 	public boolean containsKey(String key) {
 		return this.players.containsKey(key);
 	}
 	
+	/**
+	 * Put the key,info pairing into the hashmap
+	 * @param key
+	 * @param info
+	 */
 	public void put(String key, GameInfo info) {
 		this.players.put(key, info);
 	}
 	
+	/**
+	 * Get the GameInfo from the map associated with key
+	 * @param key
+	 * @return
+	 */
 	public GameInfo get(String key) {
 		return this.players.get(key);
 	}
