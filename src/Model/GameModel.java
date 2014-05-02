0a051c05c54237a308afcb5c5c7971963c2815ad
Carolyn Shuford <sockcreature@gmail.com>
2014-05-01 21:30:26 -0500

    Documentation on GameModel

--- src/Model/GameModel.java
+++ src/Model/GameModel.java
@@ -16,16 +16,26 @@ import com.google.devrel.samples.ttt.Board;
 import com.google.devrel.samples.ttt.Cell;
 import com.google.devrel.samples.ttt.CellContainer;
 
+/**
+ * The Game Model class
+ *
+ */
 public class GameModel {
 	private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	public static Key boardKey = KeyFactory.createKey("BoardKey", "MyBoard");
 
 	//public static String turnControlPath = "http://localhost:8887";
 	//public static String gameServerPath = "http://localhost:8888";
-	
+
 	public static String turnControlPath = "https://1-dot-striped-buckeye-555.appspot.com";
 	public static String gameServerPath = "https://app405cloudgame.appspot.com";
-	
+
+
+	/**
+	 * Stores the string representing the board into the datastore
+	 * As a "Board", boardKey entity
+	 * @param board
+	 */
 	public static void storeCurrentBoard(String board) {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		Transaction tx = datastore.beginTransaction();
@@ -40,78 +50,122 @@ public class GameModel {
 		}
 		tx.commit();
 	}
-	
+
+	/**
+	 * Convenience method
+	 * Equivalent to theirPlayerName.equals("None");
+	 * @param theirPlayerName
+	 * @return
+	 */
 	public static boolean bad(String theirPlayerName){
-        if(theirPlayerName.equals("None"))
-            return true;
-        return false;
-    }
-	
-	  public static boolean checkIfStuck(String cells[][], int r, int c){
-	        if(r == 0 && c == 0){
-	            if(bad(cells[r+1][c]) && bad(cells[r][c+1]))
-	                return true;
-	        }
-	        if(r == cells.length - 1 && c == 0){
-	            if(bad(cells[r-1][c]) &&  bad(cells[r][c+1]))
-	                return true;
-	        }
-	        if(r == 0 && c == cells[r].length - 1){
-	            if(bad(cells[r][c-1]) && bad(cells[r+1][c]))
-	                return true;
-	        }
-	        if(r == cells.length - 1 && c == cells[r].length - 1){
-	            if(bad(cells[r][c-1]) && bad(cells[r-1][c]))
-	                return true;
-	        }
-	        
-	        if(r > 0 && r < cells.length - 1 && c == 0){
-	            if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c+1]))
-	                return true;
-	        }
-	        
-	        if(r > 0 && r < cells.length - 1 && c == cells[r].length - 1){
-	            if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c-1]))
-	                return true;
-	        }
-	        
-	        if(r == 0 && c > 0 && c < cells[r].length - 1){
-	            if(bad(cells[r][c-1]) && bad(cells[r][c+1]) && bad(cells[r+1][c])){
-	                return true;
-	            }
-	        }
-	        
-	        if(r == cells.length - 1 && c > 0 && c < cells[r].length - 1){
-	            if(bad(cells[r][c-1]) && bad(cells[r][c+1]) && bad(cells[r-1][c]))
-	                return true;
-	        }
-	        
-	        if(r > 0 && r < cells.length - 1 && c > 0 && c < cells[r].length - 1){
-	            if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c-1]) && bad(cells[r][c+1]))
-	                return true;
-	        }
-	        return false;
-	    }
-	    
-	    protected static boolean isValidMove(String[][] cells, int oldRow, int oldCol, int newRow, int newCol){
-	        if(cells[newRow][newCol].equals("None"))
-	            return false;
-	        //System.err.println("xDiff is: " + xDiff + " and yDiff is: " + yDiff);
-	        if(checkIfStuck(cells, oldRow, oldCol))
-	            return true;
-	        
-	        
-	        int xDiff = Math.abs(newRow - oldRow);
-	        int yDiff = Math.abs(newCol - oldCol);
-	        
-	        if (yDiff + xDiff == 1) {
-	            return true;
-	        }
-	        
-	        return false;
-	    }
-	
-	    
+		if(theirPlayerName.equals("None"))
+			return true;
+		return false;
+	}
+
+	/**
+	 * Checks if a player at row 'r' and col 'c' has any valid adjacent moves
+	 * @param cells
+	 * @param r
+	 * @param c
+	 * @return
+	 */
+	public static boolean checkIfStuck(String cells[][], int r, int c){
+		
+		//Check if stuck in upper left corner
+		if(r == 0 && c == 0){
+			if(bad(cells[r+1][c]) && bad(cells[r][c+1]))
+				return true;
+		}
+		
+		//Check if stuck in bottom left corner
+		if(r == cells.length - 1 && c == 0){
+			if(bad(cells[r-1][c]) &&  bad(cells[r][c+1]))
+				return true;
+		}
+		
+		//Check if stuck in top right corner
+		if(r == 0 && c == cells[r].length - 1){
+			if(bad(cells[r][c-1]) && bad(cells[r+1][c]))
+				return true;
+		}
+		
+		//Check if stuck in bottom right corner
+		if(r == cells.length - 1 && c == cells[r].length - 1){
+			if(bad(cells[r][c-1]) && bad(cells[r-1][c]))
+				return true;
+		}
+
+		//Check if stuck in left-most column but not in corner
+		if(r > 0 && r < cells.length - 1 && c == 0){
+			if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c+1]))
+				return true;
+		}
+
+		//Check if stuck in right-most column but not in corner
+		if(r > 0 && r < cells.length - 1 && c == cells[r].length - 1){
+			if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c-1]))
+				return true;
+		}
+		
+		//Check if stuck in top row but not in corner
+		if(r == 0 && c > 0 && c < cells[r].length - 1){
+			if(bad(cells[r][c-1]) && bad(cells[r][c+1]) && bad(cells[r+1][c])){
+				return true;
+			}
+		}
+
+		//Check if stuck in bottom row but not in corner
+		if(r == cells.length - 1 && c > 0 && c < cells[r].length - 1){
+			if(bad(cells[r][c-1]) && bad(cells[r][c+1]) && bad(cells[r-1][c]))
+				return true;
+		}
+
+		//Check if we're stuck somewhere in the middle
+		if(r > 0 && r < cells.length - 1 && c > 0 && c < cells[r].length - 1){
+			if(bad(cells[r-1][c]) && bad(cells[r+1][c]) && bad(cells[r][c-1]) && bad(cells[r][c+1]))
+				return true;
+		}
+		
+		//We must not be stuck
+		return false;
+	}
+
+	/**
+	 * Check if a given move is valid based on a player's current location
+	 * @param cells
+	 * @param oldRow	the current row of the player
+	 * @param oldCol	the current column of the player
+	 * @param newRow	the row of the proposed move
+	 * @param newCol	the column of the proposed move
+	 * @return
+	 */
+	protected static boolean isValidMove(String[][] cells, int oldRow, int oldCol, int newRow, int newCol){
+		
+		//Check if proposed move location is available
+		if(cells[newRow][newCol].equals("None"))
+			return false;
+		
+		//If player is stuck, any move is valid
+		if(checkIfStuck(cells, oldRow, oldCol))
+			return true;
+
+		//Check if move is adjacent to current location
+		int xDiff = Math.abs(newRow - oldRow);
+		int yDiff = Math.abs(newCol - oldCol);
+		
+		if (yDiff + xDiff == 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Converts from an ArrayList of Cell objects to a two dimensional string matrix
+	 * @param cells
+	 * @return
+	 */
 	public static String[][] convertFromArrayListToMatrix(ArrayList<Cell> cells){
 		String[][] cellMatrix = new String[3][3];
 		for(int i = 0; i < 3; i++){
@@ -121,33 +175,51 @@ public class GameModel {
 		}
 		return cellMatrix;
 	}
-	
+
+	/**
+	 * Get a list of all valid moves on the board for a specific player
+	 * @param playerID
+	 * @param board
+	 * @return
+	 */
 	public static ArrayList<String> getValidMovesForPlayer(Long playerID, String board) {
-		Key playerKey = KeyFactory.createKey("PlayerList", "MyPlayerList");
-	    Query query = new Query("Player", playerKey);
-	    List<Entity> playerList = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(500));
-	    String playerName = null;
-	    for (Entity existingEntity : playerList) {
-	    	if(((Long)existingEntity.getProperty("playerID")).equals(playerID)){
-	    		playerName = (String)existingEntity.getProperty("playerName");
-	    	}
-	    }
-	    if(playerName == null)
-	    	return new ArrayList<String>();
-	    CellContainer cellContainer = CellContainer.fromJson(board);
-		ArrayList<Cell> cells = cellContainer.cells;
 		
+		//Get the list of players from the datastore 
+		Key playerKey = KeyFactory.createKey("PlayerList", "MyPlayerList");
+		Query query = new Query("Player", playerKey);
+		List<Entity> playerList = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(500));
+		
+		//Find the playerName associated with the paramater playerID
+		String playerName = null;
+		for (Entity existingEntity : playerList) {
+			if(((Long)existingEntity.getProperty("playerID")).equals(playerID)){
+				playerName = (String)existingEntity.getProperty("playerName");
+			}
+		}
+		
+		//If no playerName found, no valid moves to return
+		if(playerName == null)
+			return new ArrayList<String>();
+		
+		
+		CellContainer cellContainer = CellContainer.fromJson(board);
+		ArrayList<Cell> cells = cellContainer.cells;
+
+		//Find the current position of the player
 		Cell currPos = new Cell();
 		for (Cell cell : cells ) {
 			if (cell.playerName.equals(playerName)){
 				currPos = cell;
 			}
 		}
-		
+
+		//Find all valid move options for the player
 		String[][] cellsMatrix = convertFromArrayListToMatrix(cells);
 		ArrayList<String> validMoves = new ArrayList<String>();
+		
 		for (Cell cell : cells ) {
 			if(isValidMove(cellsMatrix, currPos.x, currPos.y, cell.x, cell.y)){
+				//If this cell is a valid move, create a new board to represent that move
 				ArrayList<Cell> newBoard = cells;
 				for(Cell subCell : newBoard){
 					if(subCell.x == cell.x && subCell.y == cell.y){
@@ -157,7 +229,7 @@ public class GameModel {
 				validMoves.add(CellContainer.toJson(new CellContainer(newBoard)));
 			}
 		}
-		
+
 		return validMoves;
 	}
 }
